c89d1b01e416e35456b44079d569d1508f53f829
anujb6 <bhoranuj6@gmail.com>
2022-04-14 15:10:16 +0530

    First .net API

---
+++ WebAPI2/Controllers/IssueController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI2.Data;
+using WebAPI2.Models;
+
+namespace WebAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //Get request
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get() => await _context.Issues.ToListAsync();
+
+        //Get request By Id
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issues = await _context.Issues.FindAsync(id);
+            return issues == null ? NotFound() : Ok(issues);
+        }
+
+        //Post request
+        [HttpPost]
+        public async Task<IActionResult> Post(Issue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+
+            return  CreatedAtAction(nameof(GetById), new {id = issue.Id}, issue);
+        }
+
+        //Edit data by id
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Issue issue)
+        {
+            if (id != issue.Id) return BadRequest();
+
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //Delete data by Id
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var issueToDelete = await _context.Issues.FindAsync(id);
+            if (issueToDelete == null) return NotFound();
+
+            _context.Issues.Remove(issueToDelete);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+            
+        }
+    }
+}
---
+++ WebAPI2/Data/IssueDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI2.Models;
+
+namespace WebAPI2.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options): base(options) { }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
